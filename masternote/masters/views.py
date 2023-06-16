fc1d20178bf8bd7f178a9727395c9219491e8c31
pratik <pratikrathodc2000gmail.com>
2023-06-16 21:24:39 +0530

    test vercel

--- masternote/masters/views.py
+++ masternote/masters/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def test(request):
-    res = "<h1> hello world check rpasojd salkdjasldkjas asdkjask</h1>"
+    res = "<h1> hello world check LMAO</h1>"
     return HttpResponse(res)
 
