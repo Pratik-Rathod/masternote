387b7f3f2b96b03266d999b05b5631f1e90d8d81
pratik <pratikrathodc2000gmail.com>
2023-06-30 20:11:17 +0530

    Login-form

--- masternote/masters/views.py
+++ masternote/masters/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth import login, logout,authenticate
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -15,14 +16,13 @@ def userlogin(request):
       password = request.POST.get('password')
       
       user  = authenticate(username=username,password=password)
-      
       if user is not None:
          login(request,user)
          return redirect('home')
       else:
+         messages.error(request, "~Username or password was wrong")
          return redirect('login')
    else:    
-      context['login'] = 'Under contruction'
+      context['login'] = AuthenticationForm()
 
-   print(context) 
    return render(request,'masters/login.html',context)
