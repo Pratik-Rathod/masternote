bcab143ac96395103f385106b419a55ef3feeaeb
pratik <pratikrathodc2000gmail.com>
2023-07-04 18:01:32 +0530

    build

--- masternote/build_files.sh
+++ masternote/build_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python -m venv venv
+source venv/bin/activate
+
 # Install the latest version of pip
 echo "Installing the latest version of pip..."
 python -m pip install --upgrade pip
@@ -8,10 +13,10 @@ python -m pip install --upgrade pip
 echo "Building the project..."
 python -m pip install -r requirements.txt
 
-# # Make migrations
-# echo "Making migrations..."
-# python manage.py makemigrations --noinput
-# python manage.py migrate --noinput
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
