0538a98055aff9dee20b1d176bd35c4bdbf1a38c
pratik <pratikrathodc2000gmail.com>
2023-07-04 20:40:14 +0530

    fix

--- masternote/masternote/settings.py
+++ masternote/masternote/settings.py
@@ -127,11 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-else:
-    STATIC_ROOT = os.path.join(
-        BASE_DIR, 'staticfiles', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
