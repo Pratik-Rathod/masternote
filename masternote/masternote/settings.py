c9f00ecb58953b007f73850196d23670cdfdea24
pratik <pratikrathodc2000gmail.com>
2023-07-04 18:23:48 +0530

    fixes

--- masternote/masternote/settings.py
+++ masternote/masternote/settings.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'masternote.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { }
 
 
 # Password validation
