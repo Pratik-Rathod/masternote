066e71cf41b88e01974c9f5956cefa6484ada894
pratik <pratikrathodc2000gmail.com>
2023-07-19 03:11:27 +0530

    security-fix

--- masternote/masternote/urls.py
+++ masternote/masternote/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+import os
+
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('masters.urls')),
     path('', include('notes.urls')),
 ]
 
+if DEBUG:
+    urlpatterns += [path('admin/', admin.site.urls),]
